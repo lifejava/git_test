b7532096780b01da9e05a5e83d565ee4be1419f9
lifejava <vorkout2013@gmail.com>
2016-05-07 14:49:56 +0300

    lesson 2

---
+++ src/lesson2/CalcInit.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class CalcInit{
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    calculator.add(20, 10);
+    calculator.substruct(20, 10);
+    calculator.div(20, 10);
+    calculator.multiple(20, 10);
+  }
+}
