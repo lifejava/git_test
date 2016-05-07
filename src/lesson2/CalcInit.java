82f50ce7bd1956399828870772cb6aef907fe5fb
lifejava <vorkout2013@gmail.com>
2016-05-07 20:37:05 +0300

    delete System.out.println()

--- src/lesson2/CalcInit.java
+++ src/lesson2/CalcInit.java
@@ -2,10 +2,19 @@ package lesson2;
 
 public class CalcInit{
   public static void main(String[] args) {
+  
     Calculator calculator = new Calculator();
+    
     calculator.add(20, 10);
+    calculator.getResult();
+    
     calculator.substruct(20, 10);
+    calculator.getResult();
+    
     calculator.div(20, 10);
+    calculator.getResult();
+    
     calculator.multiple(20, 10);
+    calculator.getResult();
   }
 }
--- src/lesson2/Calculator.java
+++ src/lesson2/Calculator.java
@@ -5,22 +5,22 @@ public class Calculator{
   
   public void add(double first, double second) {
     this.result = first + second;
-    System.out.println(this.result);
   }
   
   public void substruct(double first, double second) {
     this.result = first - second;
-    System.out.println(this.result);
   }
   
   public void div(double first, double second) {
     this.result = first / second;
-    System.out.println(this.result);
   }
   
   public void multiple(double first, double second) {
     this.result = first * second;
-    System.out.println(this.result);
+  }
+  
+  public void getResult(){
+    System.out.println(result);
   }
   
 }
