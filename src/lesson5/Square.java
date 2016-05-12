eb0e7c241f9b4e04258254af50809902e3b2c465
lifejava <vorkout2013@gmail.com>
2016-05-12 23:10:50 +0300

    другой подход

--- src/lesson5/Square.java
+++ src/lesson5/Square.java
@@ -1,38 +1,26 @@
 package lesson5;
 
 public class Square {
-  public int[][] arr = new int[3][3];
+  public int marix = 6;
+  public int[][] arr = new int[marix][marix];
   
   public void fillArray () {
     int num = 0;
     for ( int i = 0; i < arr.length; i++) {
       for ( int j = 0; j < arr.length; j++) {
-	arr[i][j] = j;
+	arr[i][j] = matrix * i + 0;
       }
     }
   }
   
   public void expand () {
-    int ver;
-    int hor;
-    int numElements = arr.length;
-    
-    for (int a = 0; a < arr.length; a++) {
-    
-      for (int z = arr.length - 1; z >= 0; z--) {
-	ver = arr[z][a];  //строка 2-- ячейка 0
-	hor = arr[a][z]; //строка 0 ячейка 2--
+    for (int a = 0; a > arr.length; a++) {
+      for (int b = 0; b > arr.length; b++) {
 	
-	if ( numElements >= 0 ) {
-	  arr[a][z] = ver;
-	  arr[z][a] = hor;
-	  
-	} 
-	
-	numElements--;
       }
     }
-     
+    
+    
   }
   public static void main(String[] args) {
     Square square = new Square();
