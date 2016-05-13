f335d95cbd41d19b52168b8666861fd55fa53b3f
lifejava <vorkout2013@gmail.com>
2016-05-13 11:05:14 +0300

    вспомогательный массив для разворота

--- src/lesson5/Square.java
+++ src/lesson5/Square.java
@@ -1,45 +1,42 @@
 package lesson5;
 
 public class Square {
-  private int marix = 6;
-  private int[][] arr = new int[marix][marix];
+  private int m = 6;
+  public int[][] arr = new int[m][m];
   
   public void fillArray () {
-     for (int k=0; k<m/2; k++) // border -> center
-        {
-            for (int j=k; j<m-1-k; j++) // left -> right
-            {
-                // меняем местами 4 угла
-                int tmp         arr A[k][j];
-                arr[k][j]         arr A[j][m-1-k];
-                arr[j][m-1-k]     arr A[m-1-k][m-1-j];
-                arr[m-1-k][m-1-j] arr A[m-1-j][k];
-                arr[m-1-j][k]     arr tmp;
-            }
-        }
+    int num = 0;
+    for ( int i = 0; i < arr.length; i++) {
+      for ( int j = 0; j < arr.length; j++) {
+	arr[i][j] = j;
+      }
+    }
   }
   
   public void expand () {
-    for (int a = 0; a > arr.length; a++) {
-      for (int b = 0; b > arr.length; b++) {
-	
+    int[][] pilot = new int[m][m];
+    
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0, p = arr.length - 1; j < arr.length; j++, p--) {
+	pilot[i][p] = arr[j][i];
       }
     }
-    
-    
-  }
+    arr = pilot;
+   }    
+
   public static void main(String[] args) {
     Square square = new Square();
     square.fillArray();
-    for ( int i = 0; i < square.arr.length; i++) {
-      for ( int j = 0; j < square.arr.length; j++) {
-	System.out.print(square.arr[i][j]);
-      }
-      System.out.println();
-    }  
+    //System.out.println("---------------Не развернут---------------");
+    //for ( int i = 0; i < square.arr.length; i++) {
+      //for ( int j = 0; j < square.arr.length; j++) {
+	//System.out.print(square.arr[i][j]);
+      //}
+      //System.out.println();
+    //}  
     
     square.expand();
-    System.out.println("------------------------------");
+    //System.out.println("---------------Разворачиваем---------------");
     for ( int i = 0; i < square.arr.length; i++) {
       for ( int j = 0; j < square.arr.length; j++) {
 	System.out.print(square.arr[i][j]);
